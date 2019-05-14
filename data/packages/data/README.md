fd338e324144a497ab7810db652ec23072a4c41e
iampeterbanjo <peterbanjo@gmail.com>
2019-05-14 06:24:39 +0100

    add and update README

---
+++ data/packages/data/README.md
@@ -0,0 +1,8 @@
+# Data
+
+Storage for API and data transformation tasks
+
+## Development
+
+- `yarn start` runs data import scripts
+- `yarn test` runs tests
---
+++ data/packages/data/__tests__/start.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('@hapi/code');
+const Lab = require('@hapi/lab');
+
+const lab = Lab.script();
+const { test, suite } = lab;
+
+exports.lab = lab;
+
+suite('data', () => {
+	expect('not implemented').to.equal('true');
+});
